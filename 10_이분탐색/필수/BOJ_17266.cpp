04f51988420e9610b291965c94cf5c785bd8ac44
HongYeonLee <llhongyan@naver.com>
2024-11-05 15:14:55 +0900

    [이분탐색] 11월 5일

---
+++ 10_이분탐색/필수/BOJ_17266.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> position;
+
+bool findHeight(int height) {
+    if (position[0] - height > 0) {
+        return false;
+    }
+
+    for (int i = 1; i < m; i++) {
+        if (position[i] - height > position[i - 1] + height) {
+            return false;
+        }
+    }
+
+    if (light[m - 1] + height < n){
+        return false;
+    }
+    return true;
+}
+
+int binarySearch() {
+    int left = 0;
+    int right = n;
+    int mid, ans = -1;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
+        if (findHeight(mid)) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    // 입력
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int input;
+        cin >> input;
+        position.push_back(input);
+    }
+
+    // 연산 && 출력
+    cout << binarySearch();
+
+    return 0;
+}
